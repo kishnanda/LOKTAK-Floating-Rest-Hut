ee2822909298a523ca62115a51203204775addf4
kishnanda <47675709+kishnanda@users.noreply.github.com>
2019-08-10 23:18:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('cache-polyfill').then(function(cache) {
+     return cache.addAll([
+       '/',
+       'index.html',
+       'img/ABOUTHUT.png',
+      'img/headerSlider1.jpeg',
+      'img/headerSlider1.jpeg.jpg',
+      'img/headerSlider2.jpeg.jpg',
+       'js/smooth-scroll.js',
+       'style.css',
+       'index.html',
+     
+     ]);
+   })
+ );
+});
 
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
 
-
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
