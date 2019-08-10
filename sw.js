23e458cf7dd9ac65d8b117204a8a0ce2dfe95217
kishnanda <47675709+kishnanda@users.noreply.github.com>
2019-08-10 23:46:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,4 @@
-    importScripts('cache-polyfill.js');
-
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       'index.html',
-         'style.css',
-         'style.css',
-          'js',
-
-     ]);
-   })
- );
+self.addEventListener('install', (e) {
+   console.log("Service Worker: Installed");
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
-    })
-  );
-});
