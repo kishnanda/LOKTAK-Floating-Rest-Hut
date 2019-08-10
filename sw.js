5768caa87b2cffa7c31ca52b2506c4b82002391d
kishnanda <47675709+kishnanda@users.noreply.github.com>
2019-08-10 22:46:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('/cache-polyfill.js');
+importScripts('cache-polyfill.js');
 
 
 self.addEventListener('install', function(e) {
@@ -13,17 +13,6 @@ self.addEventListener('install', function(e) {
        '/scripts/main.min.js',
        '/sounds/airhorn.mp3'
      ]);
-   })
- );
-});
-
-
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
-
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
    })
  );
 });
