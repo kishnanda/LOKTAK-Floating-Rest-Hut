0bcdf1167b66fb1c9404dc1e2c63889fed684508
kishnanda <47675709+kishnanda@users.noreply.github.com>
2019-08-10 23:56:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+'index.html',
+   'style.css',
+   '/js/smooth-scroll.js',
+   '/img',
+];
+
 self.addEventListener('install', e => {
    console.log("Service Worker: Installed");
+   e.waitUntil(
+   caches
+      .open(cacheName)
+      .then(cache => {
+       console.log('Service worker: Caching Files');
+         cahce.addAll(cacheAssets);
+      })
+      .then(()  => self.skipwaiting())
+   );
 });
 
+self.addEventListener('activate', e => {
+   console.log("Service Worker: Activated");
+});
